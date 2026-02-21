4abd0d6f1394b7e74160fc04d013d23094066b3a
Angshuman <angshumandas2611@gmail.com>
2026-02-21 19:59:22 +0530

    Initial commit

---
+++ Grocery/GroceryApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroceryApp.swift
+//  Grocery
+//
+//  Created by Angshuman on 21/02/26.
+//
+
+import SwiftUI
+
+@main
+struct GroceryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
